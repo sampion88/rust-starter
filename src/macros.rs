a930f0f6ab49f9ae2ae8e84b47cd4dc00fe9c79d
sampion88 <36301168+sampion88@users.noreply.github.com>
2025-06-19 16:39:10 +0200

    Update macros.rs

--- src/macros.rs
+++ src/macros.rs
@@ -1,11 +1,32 @@
-#[macro_export]
-macro_rules! fmt(($token:expr) => (format!("{:?}", $token)));
+use std::fs
+use std::fs::File;
+use std::io::prelude::*;
+use tempfile::tempdir;
 
-#[cfg(test)]
-mod test {
-
-  #[test]
-  fn test_macro_works_at_all() {
-    let _ = fmt!(100);
-  }
+fn main() -> std::io::Result<()> {
+    // ok: AIK_Rust-hardcoded-tmp-dir
+    let safe_dir = tempdir()?;
+    // ok: AIK_Rust-hardcoded-tmp-dir
+    let safe_dir2 = fs::create_dir("/public");
+    // ruleid: AIK_Rust-hardcoded-tmp-dir
+    let dir = fs::create_dir_all("/tmp/my_app_temp_dir")?;
+    // ruleid: AIK_Rust-hardcoded-tmp-dir
+    let dir2 = fs::create_dir("/tmp")?;
+    // ruleid: AIK_Rust-hardcoded-tmp-dir
+    let mut file_1 = File::create_new("/tmp/foo.txt")?;
+    file_1.write_all("Hello, world!".as_bytes())?;
+    // ok: AIK_Rust-hardcoded-tmp-dir
+    let mut safe_file_1 = File::create_new("foo.txt")?;
+    safe_file_1.write_all("Hello, world!".as_bytes())?;
+    // ruleid: AIK_Rust-hardcoded-tmp-dir
+    let mut file_2 = File::create("/TMP/foo.txt")?;
+    file_2.write_all(&1234_u32.to_be_bytes())?;
+    // ruleid: AIK_Rust-hardcoded-tmp-dir
+    let mut file_3 = File::create_buffered("/temp/foo.txt")?;
+    assert!(file_3.capacity() > 0);
+    for i in 0..100 {
+        writeln!(&mut file_3, "{i}")?;
+    }
+    file_3.flush()?;
+    Ok(())
 }
